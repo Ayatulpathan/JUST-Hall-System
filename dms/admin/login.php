de666e04aef5de6647968ef89c882fa9a3b68979
Ayatulpathan <180136.cse@gmail.com>
2022-07-05 16:32:27 +0600

    information added

--- dms/admin/login.php
+++ dms/admin/login.php
@@ -25,7 +25,7 @@
   <!-- /.login-logo -->
   <div class="card card-maroon my-2">
     <div class="card-body">
-      <p class="login-box-msg">Please enter your credentials</p>
+      <p class="login-box-msg">Please enter your information</p>
       <form id="login-frm" action="" method="post">
         <div class="input-group mb-3">
           <input type="text" class="form-control" name="username" autofocus placeholder="Username">
